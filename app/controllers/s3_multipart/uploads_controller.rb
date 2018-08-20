3d5d32345e0a4d7c9b6557fed063a52e50dfd4ca
Jeff Schuil <jeff@yourelevation.com>
2018-08-20 12:02:26 -0700

    Use strong parameters for Rails 5.1 compatability
    Hopefully got them all. Otherwise could just permit! all of the params.

--- app/controllers/s3_multipart/uploads_controller.rb
+++ app/controllers/s3_multipart/uploads_controller.rb
@@ -3,7 +3,7 @@ module S3Multipart
 
     def create
       begin
-        upload = Upload.create(params, session)
+        upload = Upload.create(upload_params, session)
         upload.execute_callback(:begin, session)
         response = upload.to_json
       rescue FileTypeError, FileSizeError => e
@@ -17,16 +17,16 @@ module S3Multipart
     end
 
     def update
-      return complete_upload if params[:parts]
-      return sign_batch if params[:content_lengths]
-      return sign_part if params[:content_length]
+      return complete_upload if upload_params[:parts]
+      return sign_batch if upload_params[:content_lengths]
+      return sign_part if upload_params[:content_length]
     end
 
     private
 
       def sign_batch
         begin
-          response = Upload.sign_batch(params)
+          response = Upload.sign_batch(upload_params)
         rescue => e
           logger.error "EXC: #{e.message}"
           response = {error: t("s3_multipart.errors.update")}
@@ -37,7 +37,7 @@ module S3Multipart
 
       def sign_part
         begin
-          response = Upload.sign_part(params)
+          response = Upload.sign_part(upload_params)
         rescue => e
           logger.error "EXC: #{e.message}"
           response = {error: t("s3_multipart.errors.update")}
@@ -48,8 +48,8 @@ module S3Multipart
 
       def complete_upload
         begin
-          response = Upload.complete(params)
-          upload = Upload.find_by_upload_id(params[:upload_id])
+          response = Upload.complete(upload_params)
+          upload = Upload.find_by_upload_id(upload_params[:upload_id])
           upload.update_attributes(location: response[:location])
           complete_response = upload.execute_callback(:complete, session)
           response[:extra_data] = complete_response if complete_response.is_a?(Hash)
@@ -61,5 +61,15 @@ module S3Multipart
         end
       end
 
+      def upload_params
+        params.permit(
+          :id, :content_lengths, :content_length, :upload_id,
+          :uploader, :content_size, :context,
+          :content_type, :object_name, :part_number, :verb, :url,
+          headers: {},
+          upload: [:uploader, :upload_id],
+          parts: [:ETag, :partNum]
+        )
+      end
   end
 end
